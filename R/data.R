d64124c7339afdcd326eb27268f7b1c0990fa996
Nicholas Robertson <nickrrrr@gmail.com>
2023-04-06 14:48:27 +1000

    fix data docs

--- R/data.R
+++ R/data.R
@@ -1,8 +1,11 @@
 #' Example of scRNA-seq data
 #'
-#' This is a subsampled version of the melanoma patients dataset as used in our manuscript.
-#' The original dataset is available at <https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE120575>.
-#'  
+#' This is a subsampled version of the melanoma patients dataset as used in our
+#' manuscript.
+#' The original dataset is available at:
+#'      <https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE120575>.
+#' 
+#' @usage data("example_scrnaseq")
 #' @format ## `example_scrnaseq`
 #' A Seurat object with 3523 genes and 550 cells. 
 #' Some of the key metadata columns are: 
@@ -18,7 +21,8 @@
 #' Example of scFeatures() output
 #'
 #' This is an example output of the scFeatures() function for example_scrnaseq. 
-#' 
+#'
+#' @usage data("scfeatures_result")
 #' @format ## `scfeatures_result`
 #' A list with two dataframes. In each dataframe the columns are each patient and the rows are the feature values.
 #' The first dataframe contains the feature type "proportion_raw". 
