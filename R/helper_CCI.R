3d0aa934fa67440e8171213f817655be006f6297
Yue Cao <yue.cao.au@gmail.com>
2025-12-23 19:26:04 +1100

    error handling of empty CCI result
    Ensured proper handling of empty results.

--- R/helper_CCI.R
+++ R/helper_CCI.R
@@ -18,14 +18,18 @@ helper_CCI <- function( alldata , species, ncores = 1  ){
   }
   
   
-  # x <- unique(alldata$sample)[1]
+  # x <- unique(alldata$sample)[2]
  
-  capture.output( suppressMessages( individual_cci <- BiocParallel::bplapply(  unique(alldata$sample), function(x){
+ 
+  individual_cci <- BiocParallel::bplapply(  unique(alldata$sample), function(x){
+           
+ 
     
+    err <- try({
               this_sample_data <- alldata$data[, alldata$sample == x]
               colnames(  this_sample_data) <- make.unique( colnames(   this_sample_data) ) 
               
-              
+               
               meta = data.frame(labels = alldata$celltype[ alldata$sample == x]   )   
               rownames(meta) <-  colnames(   this_sample_data) 
           
@@ -36,54 +40,70 @@ helper_CCI <- function( alldata , species, ncores = 1  ){
               cellchat <- setIdent(cellchat, ident.use = "labels") # set "labels" as default cell identity
               
               
-              
+             
               groupSize <- as.numeric(table(cellchat@idents)) # number of cells in each cell group
            
               cellchat@DB <- CellChatDB # set the used database in the object
               
               cellchat <- subsetData(cellchat) # subset the expression data of signaling genes for saving computation cost
-              # do parallel
+             
+              
+          
+               # do parallel
               cellchat <- identifyOverExpressedGenes(cellchat)
               cellchat <- identifyOverExpressedInteractions(cellchat)
               cellchat <- smoothData(cellchat, adj = PPI)
+              
+            
               cellchat <- computeCommunProb(cellchat)
               cellchat <- computeCommunProbPathway(cellchat)
               cellchat <- aggregateNet(cellchat)
               
+              
+          
               cellchat_score <-   netVisual_bubble(   cellchat,   return.data = TRUE )   
               cellchat_score  <- cellchat_score $communication
               
-     
-              if ( nrow(  cellchat_score ) == 0 ){
-                cellchat_score <- data.frame(source = "placeholder", 
-                                             target = "placeholder",
-                                             ligand = "placeholder",
-                                             receptor = "placeholder",
-                                             prob =  "placeholder",
-                                             pval =  "placeholder",
-                                             interaction_name = "placeholder",
-                                             interaction_name_2 = "placeholder",
-                                             pathway_name = "placeholder",
-                                             annotation =  "placeholder",
-                                               evidence =  "placeholder",
-                                               source.target =  "placeholder",
-                                               prob.original =  "placeholder",
-                                             feature = "placeholder" )
-              }else{
+            
+              
                 cellchat_score$feature <- paste0(  cellchat_score$source   , "->" , 
                                                    cellchat_score$target,
                                                    "--", 
                                                   cellchat_score$ligand  , "->", 
                                                   cellchat_score$receptor)
 
-                }
-             
-              
-              
+               
+           
               cellchat_score 
               
-              
-        }, BPPARAM = BPparam) ) )
+             
+    })
+    
+    if (class(err) == "try-error"){
+ 
+        cellchat_score <- data.frame(source = "placeholder", 
+                                     target = "placeholder",
+                                     ligand = "placeholder",
+                                     receptor = "placeholder",
+                                     prob =  0,
+                                     pval = 0,
+                                     interaction_name = "placeholder",
+                                     interaction_name_2 = "placeholder",
+                                     pathway_name = "placeholder",
+                                     annotation =  "placeholder",
+                                     evidence =  "placeholder",
+                                     source.target =  "placeholder",
+                                     prob.original =  0,
+                                     feature = "placeholder" )
+        
+        
+        cellchat_score 
+    
+    }else{
+      cellchat_score 
+      }
+  
+       }, BPPARAM = BPparam)  
   
   
   
@@ -96,6 +116,7 @@ helper_CCI <- function( alldata , species, ncores = 1  ){
      temp <-   individual_cci[[i]]
      
      temp  <-    temp [, c("feature", "prob" )]
+     temp <- temp[ !is.na(temp$prob), ]
      temp <- temp[ !duplicated(temp$feature) ,]
    
      if (is.null(X)){
